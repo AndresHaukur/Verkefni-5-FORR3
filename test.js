053e434b9391002bde8ffd5a0ab21ccf9c2815cc
Andres Haukur <haukurandres@gmail.com>
2023-12-08 15:20:51 +0000

    sliders

--- test.js
+++ test.js
@@ -8,18 +8,33 @@ const method = 'POST';
 let depth_max = 25;
 let depth_min = 5;
 let end_time = "2022-08-03 15:00:00";
-event_type = ["qu"];
-fields = ["event_id", "lat", "long", "magnitude"];
-magnitude_preference = ["Mlw"];
-originating_system = ["SIL picks"];
-size_max = 6;
-size_min = 2;
-sort = [];
-start_time = "2022-08-02 23:59:59";
+let event_type = ["qu"];
+let fields = ["event_id", "lat", "long", "magnitude"];
+let magnitude_preference = ["Mlw"];
+let originating_system = ["SIL picks"];
+let size_max = 6;
+let size_min = 2;
+let sort = [];
+let start_time = "2022-08-02 23:59:59";
 
+const pluto_url = 'https://data.cityofnewyork.us/resource/64uk-42ks.json';
+getData();
 
+document.addEventListener("DOMContentLoaded", function() {
+    var range1 = document.getElementById("range1");
+    var rangeValue1 = document.getElementById("rangeValue1");
 
+    range1.addEventListener("input", function() {
+        rangeValue1.value = range1.value;
+    });
 
+    var range2 = document.getElementById("range2");
+    var rangeValue2 = document.getElementById("rangeValue2");
+
+    range2.addEventListener("input", function() {
+        rangeValue2.value = range2.value;
+    });
+});
 
 
 // Define the request body
